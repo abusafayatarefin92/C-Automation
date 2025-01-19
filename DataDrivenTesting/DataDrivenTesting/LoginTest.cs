b70a89eb55cf5744e9edc635bbbffd1d9422fc0f
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2025-01-19 16:19:33 +0600

    2025 01 19

---
+++ DataDrivenTesting/DataDrivenTesting/LoginTest.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using Xunit;
+
+namespace DataDrivenTesting
+{
+    public class LoginTest
+    {
+        [Fact]
+        public void LoginWithUsername()
+        {
+            IWebDriver driver = new EdgeDriver();
+            driver.Url = "http://eaapp.somee.com/";
+
+            driver.Manage().Window.Maximize();
+
+            driver.FindElement(By.XPath("(//a[normalize-space()='Login'])[1]")).Click();
+
+            driver.FindElement(By.XPath("(//input[@id='UserName'])[1]")).SendKeys("admin");
+
+            driver.FindElement(By.XPath("(//input[@id='Password'])[1]")).SendKeys("password");
+
+            driver.FindElement(By.XPath("(//input[@id='loginIn'])[1]")).Click();
+
+        }
+    }
+}
